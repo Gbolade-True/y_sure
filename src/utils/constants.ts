bd9170964fd45755ac0eef3e7c894cfcc1f01539
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2023-11-11 21:18:12 +0000

    - switch typeorm in package.json

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 // yarn typeorm migration:generate src/migrations -d src/data-source.ts
 // yarn typeorm migration:run -- -d src/data-source.ts 
+// psql -U postgres
+// 0467388e-8f46-463f-b7dd-8eeac9849b2a
