b3c6cbb1793bafaa0f2c51099200c807e07fac4d
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2023-11-12 11:51:41 +0000

    - use vervel db

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,12 @@ import { Init1699732888175 } from './migrations/1699732888175-Init';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_ENDPOINT,
-    port: 5432,
+    url: process.env.DB_URL,
     username: 'postgres',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // port: 5432,
+    // password: process.env.DB_PASSWORD,
+    // host: process.env.DB_ENDPOINT,
+    // database: process.env.DB_NAME,
     synchronize: false,
     logging: true,
     entities: [NylonEntity, SaleEntity, PurchaseEntity, ExpenseEntity],
