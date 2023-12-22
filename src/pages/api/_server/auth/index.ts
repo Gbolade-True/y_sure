1e56a5f67d325f27129b4c4d8b8164020ca355e6
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2023-12-22 19:24:46 +0000

    - more checks

--- src/pages/api/_server/auth/index.ts
+++ src/pages/api/_server/auth/index.ts
@@ -2,10 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export const ySureAPIGuard = async (req: NextApiRequest, res: NextApiResponse): Promise<boolean> => {
   const allowedIPs = process.env.ALLOWED_IPS ? process.env.ALLOWED_IPS.split(',') : [];
-  const clientIP = req.socket.remoteAddress || '';
+  const clientIP = (req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress) as string;
 
   if (!allowedIPs.includes(clientIP)) {
-    res.status(403).json({ error: `Unauthorized Access - Denied, ${clientIP}` });
+    res.status(401).json({ error: `Unauthorized Access - Denied, ${clientIP}` });
     return false;
   }
 
