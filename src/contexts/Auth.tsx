a16aaa72bfc168e0388d8d0ec864c21dd5c1c612
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2024-01-12 17:18:43 +0000

    - disabled client auth for Alex's tests

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,8 +1,8 @@
 // AuthContext.tsx
 import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
-import useSWR from 'swr';
-import LoadingPage from '@/components/Loading';
-import { ClientResponse } from '@/pages/api/_server/utils/constants';
+// import useSWR from 'swr';
+// import LoadingPage from '@/components/Loading';
+// import { ClientResponse } from '@/pages/api/_server/utils/constants';
 import { Result } from 'antd';
 
 type Props = {
@@ -16,28 +16,30 @@ export const AuthContext = createContext<{
 });
 
 export const useAuthContext = () => useContext(AuthContext);
+const ALEX_BYPASS = 'alex_bypass';
 
 export const AuthProvider = ({ children }: Props) => {
   const [authenticated, setAuthenticated] = useState(false);
-  const fetcher = (url: string) => fetch(url).then(res => res.json());
-  const { data: userIP, isLoading } = useSWR<ClientResponse<string>>(`/api/check_ip`, fetcher);
+  // TDisabled for Alex's tests
+  // const fetcher = (url: string) => fetch(url).then(res => res.json());
+  // const { data: userIP, isLoading } = useSWR<ClientResponse<string>>(`/api/check_ip`, fetcher);
 
   useEffect(() => {
-    if (!userIP && !isLoading) setAuthenticated(false);
+    // if (!userIP && !isLoading) setAuthenticated(false);
     // Check the IP addresses in your environment
-    const allowedIPs = process.env.NEXT_PUBLIC_ALLOWED_IPS ? process.env.NEXT_PUBLIC_ALLOWED_IPS.split(',') : [];
+    const allowedIPs = [ALEX_BYPASS];
 
-    if (!allowedIPs.includes(userIP?.data || '')) {
+    if (!allowedIPs.includes(ALEX_BYPASS)) {
       setAuthenticated(false);
       return;
     }
 
     setAuthenticated(true);
-  }, [userIP, isLoading]);
+  }, []);
 
   return (
     <AuthContext.Provider value={{ authenticated }}>
-      {isLoading && <LoadingPage />}
+      {/* {isLoading && <LoadingPage />} */}
       {authenticated ? children : <Result status="403" title="401" subTitle="Unauthorized: Access - Denied" />}
     </AuthContext.Provider>
   );
